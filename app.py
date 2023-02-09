0c63d0eda5237a9e24da54436051c062d846b829
max <988869+MerliX@users.noreply.github.com>
2023-02-09 12:25:57 +0400

    create_app

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from models import Failure
 import logging
 
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
 
 
 def setup_webhook():
@@ -18,29 +15,38 @@ def setup_webhook():
     requests.post(f'https://api.telegram.org/bot{bot_token}/setWebhook?url={bot_url}')
     logger.info("Bot webhook set")
 
-setup_webhook()
-db.create_all()
 
-@app.route('/bot/', methods=['POST'])
-def handle_update():
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-    update = request.get_json()
-    logger.info(update)
-    message = update.get('message')
-    if message and message.get('text'):
-        command, *comment = message['text'].split()
-        chat_id = message['chat']['id']
-        if command == '/failure' and comment:
-            comment = ' '.join(comment)
-            failure = Failure(comment=comment)
-            db.session.add(failure)
-            db.session.commit()
-            response = f"Failure recorded: '{comment}'"
-        else:
-            response = "Invalid command or comment"
-        bot_token = os.environ.get('BOT_TOKEN')
-        requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={response}')
-    return "ok"
+    db = SQLAlchemy(app)
+    db.create_all()
+
+    setup_webhook()
+    @app.route('/bot/', methods=['POST'])
+    def handle_update():
+
+        update = request.get_json()
+        logger.info(update)
+        message = update.get('message')
+        if message and message.get('text'):
+            command, *comment = message['text'].split()
+            chat_id = message['chat']['id']
+            if command == '/failure' and comment:
+                comment = ' '.join(comment)
+                failure = Failure(comment=comment)
+                db.session.add(failure)
+                db.session.commit()
+                response = f"Failure recorded: '{comment}'"
+            else:
+                response = "Invalid command or comment"
+            bot_token = os.environ.get('BOT_TOKEN')
+            requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={response}')
+        return "ok"
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(port=8080)
