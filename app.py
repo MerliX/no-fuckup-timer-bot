b402906a64712102484a26e9bb6c8f5b2d8172a2
max <988869+MerliX@users.noreply.github.com>
2023-02-13 20:23:40 +0400

    minutes passed human readable

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def setup_webhook():
     logger.info("Bot webhook set")
 
 
+def convert_timedelta_to_russian_string(td: datetime.timedelta) -> str:
+    days, remainder = divmod(td.total_seconds(), 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    result = []
+    if int(days) > 0:
+        result.append(f"{int(days)} день(ей)")
+    if int(hours) > 0:
+        result.append(f"{int(hours)} часа(ов)")
+    if int(minutes) > 0:
+        result.append(f"{int(minutes)} минут(ы)")
+    return " ".join(result)
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -50,23 +63,22 @@ def create_app():
             chat_id = message['chat']['id']
             response = f"Ничего не понятно (команда {command})"
             latest_failure = Failure.query.order_by(Failure.created_at.desc()).first()
-            minutes_passed = 0
+            time_passed = datetime.timedelta(0)
             if latest_failure:
                 time_passed = datetime.datetime.now() - latest_failure.created_at
-                minutes_passed = time_passed.seconds//60
             if command in ('/proeb', '/proeb@badbartimerbot') and comment:
                 comment = ' '.join(comment)
                 failure = Failure(comment=comment, user=str(message.get('from')))
                 db.session.add(failure)
                 db.session.commit()
 
-                response = f"Проеб засчитан: '{comment}'\nПродержались минут: {minutes_passed}"
+                response = f"Проеб засчитан: '{comment}'\nПродержались: {convert_timedelta_to_russian_string(time_passed)}"
             elif command in ('/proeb', '/proeb@badbartimerbot'):
                 response = "Укажи, что именно пошло не так (пример: `/proeb я покакал`)"
             elif command in ('/last', '/last@badbartimerbot'):
                 # Get the latest failure from the database
                 if latest_failure:
-                    response=f'Минут без проеба: {minutes_passed}'
+                    response=f'Без проеба: {convert_timedelta_to_russian_string(time_passed)}'
 
             bot_token = os.environ.get('BOT_TOKEN')
             requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={response}')
