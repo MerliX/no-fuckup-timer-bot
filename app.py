57adb07797000646ae130833df3f50ff38f7f33f
Max P <988869+MerliX@users.noreply.github.com>
2024-10-02 09:34:16 +0300

    template

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def create_app():
         created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
         def __repr__(self):
-            return f'<Failure id={self.id} comment={self.comment} user={self.user} created_at={self.created_at}>'
+            return f'{self.user} {self.created_at}: {self.comment}'
 
     with app.app_context():
         try:
