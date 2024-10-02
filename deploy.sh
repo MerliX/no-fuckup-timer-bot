b561b3cc80ea476eb35565cdc58ebf1802188ad4
Max P <988869+MerliX@users.noreply.github.com>
2024-10-02 09:31:24 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 # Change to the project directory
 cd /home/no-fuckup-timer-bot/
-docker-compose down
+docker compose down
 
 # Pull the latest changes from Git
 git fetch --all
 git pull
 
-docker-compose build
+docker compose build
 
 # Restart Docker Compose
 
-docker-compose up -d
+docke compose up -d
